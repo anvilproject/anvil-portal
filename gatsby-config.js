e8a03a9464c560255fbe6bd1df1864ff4c52ab3a
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2019-05-28 09:50:16 +1000

    AnVIL favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,37 @@ module.exports = {
         author: `The AnVIL team`,
     },
     plugins: [
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./images/favicon.png",
+
+                // WebApp Manifest Configuration
+                appName: null, // Inferred with your package.json
+                appDescription: null,
+                developerName: null,
+                developerURL: null,
+                dir: 'auto',
+                lang: 'en-US',
+                background: '#fff',
+                theme_color: '#fff',
+                display: 'standalone',
+                orientation: 'any',
+                start_url: '/?homescreen=1',
+                version: '1.0',
+
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    yandex: false,
+                    windows: false
+                }
+            }
+        },
         `gatsby-plugin-manifest`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-sharp`,
