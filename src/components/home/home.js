acce0d5ca3e72fa0fb3bf31b9f7b6ec39677947b
Fran McDade <franmcdade@Frans-MBP.hub>
2020-01-30 10:29:12 +1000

    Data Counts implementation. Stat styles. #158.

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -17,15 +17,14 @@ import {FeaturedWorkspacesStaticQuery} from "../../hooks/featuredWorkspacesQuery
 import {NewsStaticQuery} from "../../hooks/newsQuery";
 import {OnboardingStaticQuery} from "../../hooks/onboardingQuery";
 import {RoadMapIntroStaticQuery} from "../../hooks/roadMapIntroQuery";
-import {SubmittingStaticQuery} from "../../hooks/submittingQuery";
 import * as ScoopsService from "../../utils/scoops.service";
-import * as StatsService from "../../utils/stats.service";
 import Carousel from "../carousel/carousel";
 import GoArrow from "../goArrow/goArrow";
 import RoadMap from "../roadMap/roadMap";
 import Scoop from "../scoops/scoop";
 import SectionBody from "../section/sectionBody";
 import SectionIntro from "../section/sectionIntro";
+import Stats from "../stats/stats";
 import Workspaces from "../workspaces/workspaces";
 
 // Styles
@@ -35,14 +34,9 @@ import compStyles from "./home.module.css";
 let classNames = require("classnames");
 
 class Home extends React.Component {
-    
-    formatStats = (count) => {
-
-        return NumberFormatService.format(count, 0);
-    };
 
     render() {
-        const {events, eventsScoops, news, newsScoops, roadMapIntro, stats, workspaces} = this.props;
+        const {events, eventsScoops, news, newsScoops, roadMapIntro, workspaces} = this.props;
         const featuredNews = ScoopsService.isAnyScoopsFeatured(newsScoops);
         const featuredEvents = ScoopsService.isAnyScoopsFeatured(eventsScoops);
         return (
@@ -53,6 +47,7 @@ class Home extends React.Component {
                     <div className={compStyles.subhead}>AnVIL &ndash; an Analysis, Visualization, and Informatics Lab-space for democratizing genomic data access, sharing and computing across large genomic-related data sets.
                     </div>
                     <GoArrow><Link to="/about">Learn More</Link></GoArrow>
+                    <Stats/>
                 </div>
             </section>
             <Carousel/>
@@ -84,10 +79,9 @@ export default () => {
     const news = ScoopsService.getIntroduction(NewsStaticQuery());
     const newsScoops = ScoopsService.getScoops(NewsStaticQuery());
     const roadMap = RoadMapIntroStaticQuery();
-    const stats = StatsService.getStats(StatsStaticQuery());
     const workspaces = FeaturedWorkspacesStaticQuery();
 
     return (
-        <Home events={events} eventsScoops={eventsScoops} news={news} newsScoops={newsScoops} roadMapIntro={roadMap} stats={stats} workspaces={workspaces}/>
+        <Home events={events} eventsScoops={eventsScoops} news={news} newsScoops={newsScoops} roadMapIntro={roadMap} workspaces={workspaces}/>
     )
 }
---
+++ src/components/stats/stats.js
@@ -0,0 +1,48 @@
+/*
+ * The AnVIL
+ * https://www.anvilproject.org
+ *
+ * The AnVIL - stats component.
+ */
+
+// Core dependencies
+import React from "react";
+
+// App dependencies
+import {StatsStaticQuery} from "../../hooks/statsQuery";
+import * as NumberFormatService from "../../utils/number-format.service";
+import * as StatsService from "../../utils/stats.service";
+import Stat from "../stat/stat";
+
+// Styles
+import compStyles from "./stats.module.css";
+
+class Stats extends React.Component {
+
+    formatStats = (count) => {
+
+        return NumberFormatService.format(count, 0);
+    };
+
+    render() {
+        const {stats} = this.props;
+        return (
+            <div className={compStyles.stats}>
+                <Stat stat={this.formatStats(stats.sources)}>sources</Stat>
+                <Stat stat={this.formatStats(stats.subjects)}>subjects</Stat>
+                <Stat stat={this.formatStats(stats.samples)}>samples</Stat>
+                <Stat stat={this.formatStats(stats.projects)}>projects</Stat>
+                <Stat stat={this.formatStats(stats.size)}>size</Stat>
+            </div>
+        );
+    }
+}
+
+export default () => {
+
+    const stats = StatsService.getStats(StatsStaticQuery());
+
+    return (
+        <Stats stats={stats}/>
+    );
+}
