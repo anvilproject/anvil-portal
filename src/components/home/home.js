b95070240f6332be89cc16739d11535a2815e135
MillenniumFalconMechanic <mim@clevercanary.com>
2020-02-18 09:42:10 -0800

    Updated Terra logo size. Resolves #142.

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -52,7 +52,7 @@ class Home extends React.Component {
                     <div className={compStyles.subhead}>AnVIL &ndash; an Analysis, Visualization, and Informatics Lab-space for democratizing genomic data access, sharing and computing across large genomic-related data sets.
                     </div>
                     <GoArrow><Link to="/about">Learn More</Link></GoArrow>
-                    <GoArrow className={goStyles.stretch}><a href="https://anvil.terra.bio/#" rel="noopener noreferrer" target="_blank"><img className={compStyles.logoTerra} src={logoTerra} alt="Terra"/>Launch AnVIL in Terra</a></GoArrow>
+                    <GoArrow className={goStyles.stretch}><a href="https://anvil.terra.bio/#" className={compStyles.linkTerra} rel="noopener noreferrer" target="_blank"><img className={compStyles.logoTerra} src={logoTerra} alt="Terra"/>Launch AnVIL in Terra</a></GoArrow>
                     <Stats/>
                 </div>
             </section>
