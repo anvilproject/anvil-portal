8bb77fc708ba8b047f60ec541610d367365b86ad
MillenniumFalconMechanic <mim@clevercanary.com>
2020-02-20 09:37:38 -0800

    Updated logo page UI. Resolves #236.

--- src/components/style-guide-download-logo/style-guide-download-logo.js
+++ src/components/style-guide-download-logo/style-guide-download-logo.js
@@ -14,7 +14,7 @@ import React from "react";
 import anvil from "../../../images/brand/anvil.png";
 import anvilProject from "../../../images/brand/anvilproject.png";
 import anvilProjectOrg from "../../../images/brand/anvilprojectorg.png";
-import download from "../../../images/icon/download.png";
+import download from "../../../images/icon/download-grey.png";
 
 // Styles
 import compStyles from "./style-guide-download-logo.module.css";
@@ -24,17 +24,20 @@ class StyleGuideDownloadLogo extends React.Component {
     render() {
         return (
             <>
+                <p>
+                    The AnVIL logos and icons are available for download below.
+                </p>
                 <a className={compStyles.logo} download="anvil" href={anvil}>
-                    <img src={download} alt="download"/>
                     <img src={anvil} alt="anvil"/>
+                    <img src={download} alt="download"/>
                 </a>
                 <a className={compStyles.logo} download="anvilproject" href={anvilProject}>
-                    <img src={download} alt="download"/>
                     <img src={anvilProject} alt="anvil project"/>
+                    <img src={download} alt="download"/>
                 </a>
                 <a className={compStyles.logo} download="anvilprojectorg" href={anvilProjectOrg}>
-                    <img src={download} alt="download"/>
                     <img src={anvilProjectOrg} alt="anvil project org"/>
+                    <img src={download} alt="download"/>
                 </a>
             </>
         );
