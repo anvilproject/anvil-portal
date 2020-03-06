aae9e5fe8d050dbb2797576724f0a20cc0865afe
Fran McDade <franmcdade@Frans-MBP.hub>
2020-03-06 13:30:09 +1000

    Check against Microsoft Edge. Browser not supported page. Resolves #303.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,5 +5,47 @@
  */
 
 // Required for Edge, otherwise we get a "PerformanceObserver not defined" error 
-import '@fastly/performance-observer-polyfill/polyfill'
+require('@fastly/performance-observer-polyfill/polyfill');
 
+// Determine site browser support
+const Bowser = require("bowser");
+
+exports.onClientEntry = () => {
+
+    // Exit if path is static page for browser not supported
+    if ( window.location.pathname === "/browser-not-supported.html" || window.location.pathname === "/browser-not-supported-static.html" ) {
+
+        return;
+    }
+
+    const browser = Bowser.getParser(window.navigator.userAgent);
+
+    // List of unsupported browsers
+    const browserNotSupported = browser.satisfies({
+
+        ie: ">=6",
+        edge: "~15",
+        windows: {
+            safari: ">=1",
+        }
+    });
+
+    // List of browsers requiring an "image" only unsupoorted page due to css inadequacies.
+    const browserCSSNotSupported = browser.satisfies({
+
+        ie: "<=10",
+    });
+
+    // Redirect to static "browser not supported" page, should browser be unsupported by the AnVIL site.
+    if ( browserNotSupported ) {
+
+        if ( browserCSSNotSupported ) {
+
+            window.location.replace("/browser-not-supported-static.html");
+        }
+        else {
+
+            window.location.replace("/browser-not-supported.html");
+        }
+    }
+};
