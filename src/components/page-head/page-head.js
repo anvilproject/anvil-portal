3fe1ee26ed923a69f14324cc6ab36a0c27edf3ec
MillenniumFalconMechanic <mim@clevercanary.com>
2021-02-26 16:21:22 -0800

    Added YouTube API script. Resolves #885.

--- src/components/page-head/page-head.js
+++ src/components/page-head/page-head.js
@@ -3,6 +3,12 @@
  * https://www.anvilproject.org
  *
  * The AnVIL page head component.
+ * 
+ * <script src="https://www.youtube.com/iframe_api"> is required for GTM YouTube tracking. This script is added 
+ * automatically if landing page contains a link to a YouTube video. If landing page doesn't contain a YouTube video,
+ * the script is never added and subsequent clicks on videos are not tracked (eg when a user navigates from the home page
+ * to a page containing a video, this script is not added). By adding this script here, we can ensure that all video
+ * events are tracked, regardless of whether the landing page contains a video or not.
  */
 
 // Core dependencies
@@ -23,6 +29,7 @@ class PageHead extends React.Component {
                 {EnvironmentService.isProd() ? null : <meta name="robots" content="noindex" />}
                 <link href="https://fonts.googleapis.com/css?family=Barlow:300,400,500|Open+Sans:300,400,600,700&display=swap" rel="stylesheet"/>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"/>
+                <script src="https://www.youtube.com/iframe_api"/>
                 <html lang="en" />
             </Helmet>
         )
