bc960c154a151fdacf13de78a82bc5ae6b654e8a
Fran McDade <franmcdade@Frans-MacBook-Pro-2.local>
2022-02-14 14:32:18 +1000

    Remove spreadsheet from AnVIL catalog workflow. #1654.

--- plugins/utils/dashboard-workspaces-anvil.service.js
+++ plugins/utils/dashboard-workspaces-anvil.service.js
@@ -29,7 +29,6 @@ const { buildGapId } = require(path.resolve(
 // Template variables
 const DENY_LIST_TERMS = ["ATTRIBUTEVALUE", "N/A", "NA", "", null];
 const fileSourceAnVIL = "dashboard-source-anvil.tsv";
-const fileSourceTerra = "dashboard-source-terra.csv";
 const CONSENT_CODE_TYPE = {
   CONSORTIUM_ACCESS_ONLY: "consortium-access only",
   NOT_APPLICABLE: "not applicable",
@@ -37,17 +36,18 @@ const CONSENT_CODE_TYPE = {
 };
 const SOURCE_HEADER_KEY = {
   CONSENT_SHORT_NAME: "library:datauserestriction",
-  CONSORTIUM: "consortium",
+  CONSORTIUM: "sheet:consortium",
   CREATED_AT: "created",
   DATA_TYPES: "library:datatype.items",
-  DB_GAP_ID: "study_accession",
-  DISEASES: "library:indication",
+  DB_GAP_ID: "tagssheet:tag:tags:dbgap",
+  DISEASES: "tagssheet:library:indication",
+  PARTICIPANTS: "participantcount",
   PROJECT_ID: "name",
-  SAMPLES: "sample count",
-  SIZE: "file size",
+  SAMPLES: "samplecount",
+  SIZE: "bucketsize",
   STUDY_DESIGNS: "library:studydesign",
-  SUBJECTS: "library:numsubjects",
-  WORKSPACE: "workspace",
+  SUBJECTS: "subjectcount",
+  WORKSPACE: "name",
 };
 const SOURCE_FIELD_KEY = {
   ACCESS_TYPE: "accessType",
@@ -59,6 +59,7 @@ const SOURCE_FIELD_KEY = {
   DB_GAP_ID_ACCESSION: "dbGapIdAccession",
   [SOURCE_HEADER_KEY.DISEASES]: "diseases",
   GAP_ID: "gapId",
+  [SOURCE_HEADER_KEY.PARTICIPANTS]: "participants",
   [SOURCE_HEADER_KEY.PROJECT_ID]: "projectId",
   [SOURCE_HEADER_KEY.SAMPLES]: "samples",
   [SOURCE_HEADER_KEY.SIZE]: "size",
@@ -73,6 +74,7 @@ const SOURCE_FIELD_TYPE = {
   [SOURCE_HEADER_KEY.DATA_TYPES]: "array",
   [SOURCE_HEADER_KEY.DB_GAP_ID]: "string",
   [SOURCE_HEADER_KEY.DISEASES]: "array",
+  [SOURCE_HEADER_KEY.PARTICIPANTS]: "number",
   [SOURCE_HEADER_KEY.PROJECT_ID]: "string",
   [SOURCE_HEADER_KEY.SAMPLES]: "number",
   [SOURCE_HEADER_KEY.SIZE]: "number",
@@ -87,8 +89,10 @@ const WORKSPACE_ACCESS_TYPE = {
 };
 const WORKSPACE_CONSORTIUM = {
   CCDG: "CCDG",
-  CONVERGENT_NEURO: "Convergent Neuro",
+  CONVERGENT_NEUROSCIENCE: "Convergent Neuroscience",
   CMG: "CMG",
+  CMH: "CMH",
+  CSER: "CSER",
   EMERGE: "eMERGE",
   GTEX: "GTEx (v8)",
   HPRC: "HPRC",
@@ -105,9 +109,6 @@ const WORKSPACE_CONSORTIUM = {
  * @returns {Promise.<void>}
  */
 const getWorkspaces = async function getWorkspaces() {
-  /* Build workspace counts. */
-  const countWorkspaces = await parseSource(fileSourceTerra, ",");
-
   /* Build the workspace attributes. */
   const attributeWorkspaces = await parseSource(fileSourceAnVIL, "\t");
 
@@ -115,11 +116,7 @@ const getWorkspaces = async function getWorkspaces() {
   const studyPropertiesById = await getStudyPropertiesById(attributeWorkspaces);
 
   /* Merge the workspace data and build any additional rule based data, and associated study properties. */
-  const workspaces = buildWorkspaces(
-    attributeWorkspaces,
-    countWorkspaces,
-    studyPropertiesById
-  );
+  const workspaces = buildWorkspaces(attributeWorkspaces, studyPropertiesById);
 
   /* Return the sorted dashboard, sorted first by consortium, then by project name. */
   const keyConsortium = SOURCE_FIELD_KEY[SOURCE_HEADER_KEY.CONSORTIUM];
@@ -230,17 +227,27 @@ function buildWorkspacePropertyStudyRequestAccessUrl(
 }
 
 /**
+ * Returns the workspace subjects count calculated from the larger of either subjects or participants count.
+ *
+ * @param row
+ * @param subjectsKey
+ * @param participantsKey
+ * @returns {{}}
+ */
+function getWorkspacePropertySubjectsCount(row, subjectsKey, participantsKey) {
+  const participantsCount = row[participantsKey];
+  const subjectsCount = row[subjectsKey];
+  const subjects = Math.max(participantsCount, subjectsCount);
+  return { [subjectsKey]: subjects };
+}
+
+/**
  * Returns the merged workspace data and any additional workspace properties of interest.
  *
  * @param attributeWorkspaces
- * @param countWorkspaces
  * @param studyPropertiesById
  */
-function buildWorkspaces(
-  attributeWorkspaces,
-  countWorkspaces,
-  studyPropertiesById
-) {
+function buildWorkspaces(attributeWorkspaces, studyPropertiesById) {
   return attributeWorkspaces.reduce((acc, row) => {
     /* Grab the study id. */
     const studyId = getWorkspaceStudyId(row);
@@ -280,9 +287,6 @@ function buildWorkspaces(
     /* Build the property gapId. */
     const propertyGapId = buildWorkspacePropertyGapId(studyId, "", studyUrl);
 
-    /* Build the property counts. */
-    const countWorkspace = findCountWorkspace(row, countWorkspaces);
-
     /* Reformat the property studyDesigns. */
     const propertyStudyDesigns = reformatWorkspacePropertyList(
       row,
@@ -299,15 +303,21 @@ function buildWorkspaces(
       dbGapIdAccession
     );
 
+    /* Grab the property subjects or participants count. */
+    const propertySubjects = getWorkspacePropertySubjectsCount(
+      row,
+      SOURCE_FIELD_KEY[SOURCE_HEADER_KEY.SUBJECTS],
+      SOURCE_FIELD_KEY[SOURCE_HEADER_KEY.PARTICIPANTS]
+    );
+
     /* Grab the workspace file size. */
     const keyFileSize = SOURCE_FIELD_KEY[SOURCE_HEADER_KEY.SIZE];
-    const size = countWorkspace[keyFileSize];
+    const size = row[keyFileSize];
 
     /* Only include workspace if there is a file size. */
     if (size && size > 0) {
       /* Merge properties. */
       const workspace = {
-        ...countWorkspace,
         ...row,
         ...propertyAccessType,
         ...propertyConsentShortName,
@@ -315,6 +325,7 @@ function buildWorkspaces(
         ...propertyDataTypes,
         ...propertyDiseases,
         ...propertyGapId,
+        ...propertySubjects,
         ...propertyStudyDesigns,
         ...propertyStudyRequestAccessUrl,
         ...propertyStudySlug,
@@ -327,53 +338,6 @@ function buildWorkspaces(
 
     return acc;
   }, []);
-}
-
-/**
- * Returns the counts for the specified workspace.
- *
- * @param row
- * @param countWorkspaces
- * @returns {*}
- */
-function findCountWorkspace(row, countWorkspaces) {
-  /* Grab the project id key. */
-  const keyProjectId = SOURCE_FIELD_KEY[SOURCE_HEADER_KEY.PROJECT_ID];
-
-  /* Grab the project id value. */
-  const projectId = row[keyProjectId];
-
-  /* Find the corresponding workspaces counts. */
-  const countWorkspace = countWorkspaces.find(
-    (countWorkspace) =>
-      countWorkspace[keyProjectId].toLowerCase() === projectId.toLowerCase()
-  );
-
-  /* Return the workspace counts if they exist. */
-  if (countWorkspace) {
-    return countWorkspace;
-  }
-
-  /* If there is no corresponding workspaces count, return the workspace count properties with a corresponding value of zero. */
-  const properties = getWorkspaceProperties(countWorkspaces);
-
-  return properties.reduce(
-    (acc, property) => Object.assign(acc, { [property]: 0 }),
-    {}
-  );
-}
-
-/**
- * Returns the properties of the specified workspace.
- *
- * @param workspaces
- * @returns {Array}
- */
-function getWorkspaceProperties(workspaces) {
-  const clonedWorkspaces = [...workspaces];
-  const workspace = clonedWorkspaces.pop();
-
-  return Object.keys(workspace);
 }
 
 /**
