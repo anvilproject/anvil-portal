504506da880e5008252ef7e592c51a4b9c0a48f6
Arctenik <17056247+Arctenik@users.noreply.github.com>
2022-04-14 18:55:55 -0700

    Update data-ingestion-chart and add update script

---
+++ scripts/update-ingestion-chart.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const fsPromises = require("fs/promises");
+const { promisify } = require("util");
+const parseCsv = promisify(require("csv-parse").parse);
+
+const workspaceFilesPath = "./workspace-files";
+const workspacesInfoPath = "../plugins/utils/dashboard-source-anvil.tsv";
+const outPath = "../src/components/data-ingestion-chart/chart-data.ts";
+
+generateChartData().then(() => {
+	console.log("Done");
+}).catch(e => {
+	throw e;
+});
+
+async function generateChartData() {
+	const workspaces = await getWorkspaces();
+	const workspaceFileNames = await getWorkspaceFileNames();
+	
+	const entriesByConsortium = {};
+	let minDateNum = Infinity;
+	let maxDateNum = -Infinity;
+	
+	for (let ws of workspaces) {
+		// let entries;
+		if (workspaceFileNames.includes(ws.name)) {
+			const entries = await getWorkspaceEntries(ws.name);
+		// } else {
+		// 	entries = [ws];
+		// }
+			const arr = (entriesByConsortium[ws.consortium] || (entriesByConsortium[ws.consortium] = []));
+			entries.forEach(e => {
+				arr.push(e);
+				const dateNum = new Date(e.date).getTime();
+				if (dateNum < minDateNum) minDateNum = dateNum;
+				if (dateNum > maxDateNum) maxDateNum = dateNum;
+			});
+		} else {
+			console.log("Missing: " + ws.name);
+		}
+	}
+	
+	const minDate = new Date(minDateNum);
+	const startYear = minDate.getFullYear();
+	const startMonth = minDate.getMonth();
+	const numMonths = dateToIndex(maxDateNum) + 1;
+	const monthDataByConsortium = Object.entries(entriesByConsortium).map(([consortium, entries]) => {
+		const addedSizes = Array(startMonth + 1 + numMonths).fill(0); // that +1 is to get the labels aligned
+		
+		for (let e of entries) {
+			addedSizes[dateToIndex(e.date) + startMonth + 1] += parseFloat(e.size); // that one too
+		}
+		
+		return [consortium, addedSizes];
+	});
+	
+	const scriptText = `export const startYear = ${JSON.stringify(startYear)}; export const monthDataByConsortium: Array<[string, Array<number>]> = ${JSON.stringify(monthDataByConsortium)};`;
+	
+	fsPromises.writeFile(outPath, scriptText);
+	
+	
+	function dateToIndex(dateSrc) {
+		let date = new Date(dateSrc);
+		return (date.getFullYear() - startYear) * 12 - startMonth + date.getMonth();
+	}
+}
+
+async function getWorkspaces() {
+	let [fields, ...data] = await parseCsv(await fsPromises.readFile(workspacesInfoPath, "utf8"), {delimiter: "\t", ltrim: true});
+	return data.map(items => Object.fromEntries(fields.map((name, i) => [name, items[i]])));
+}
+
+async function getWorkspaceFileNames() {
+	return (await fsPromises.readdir(workspaceFilesPath)).map(n => n.replace(/\.csv$/i, ""));
+}
+
+async function getWorkspaceEntries(name) {
+	let arr = await parseCsv(await fsPromises.readFile(path.resolve(workspaceFilesPath, name + ".csv"), "utf8"));
+	if (arr[arr.length - 1][1] === "TOTAL:") arr.pop();
+	return arr.map(([date, size]) => ({date, size: size/1e12}));
+}
