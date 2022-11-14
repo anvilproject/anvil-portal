76cccf967a727e455a47b5fa8bf6c2f143c9f1fb
Dave Rogers <dave@clevercanary.com>
2022-11-14 10:26:42 -0800

    Update data ingest over time graph (#2351)

--- scripts/workspace-files.sh
+++ scripts/workspace-files.sh
@@ -1,20 +1,19 @@
 row="$1"
-#bucketName=$(cut -d , -f 3 <<< "$row")
-#workspaceName=$(cut -d , -f 1 <<< "$row")
-bucketName=$(cut -f 9 <<< "$row")
+bucketName=$(cut -f 10 <<< "$row")
 workspaceName=$(cut  -f 1 <<< "$row")
 
-gsutil -u anvil-and-terra-development ls -l  gs://"$bucketName"/** | while read -r line ; do
+echo "$workspaceName"
+echo "$bucketName"
 
+gsutil -u anvil-and-terra-development ls -l  gs://"$bucketName"/** | while read -r line ; do
     trimed=$(tr -s ' ' <<< "$line")
     bytes=$(cut -d ' ' -f 1 <<< "$trimed")
     date=$(cut -d  ' ' -f 2 <<< "$trimed")
     out=${date}\,${bytes}
-    echo "$out" >> "$workspaceName".csv
+    echo "$out" >> workspace-files/"$workspaceName".csv
 done
 
 #gsutil -u anvil-and-terra-development ls -l  gs://"$bucketName"/**   | cut -d , -f 1 > "$workspaceName".csv
 
-#while read in; do ../workspace-files.sh "$in"; done < ../output-2021-11-15.csv
-
-#while read in; do ../workspace-files.sh "$in"; done < ../AnVILCatalogWorkspacesPublicMetadataFromTerraAPI-2022-03-15.csv
+#Last run command
+# while read in; do ./workspace-files.sh "$in"; done < ../AnVILCatalogWorkspacesPublicMetadataFromTerraAPI-2022-11-07.tsv
