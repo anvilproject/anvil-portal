bd62bcfff4e0154cee3aaf60c7569b6eabbb8c1c
David Rogers <dave@clevercanary.com>
2023-03-13 12:15:11 -0700

    Update cgl-anvil-dev-deploy.sh

--- cgl-anvil-dev-deploy.sh
+++ cgl-anvil-dev-deploy.sh
@@ -6,5 +6,5 @@
 
 export BUCKET=s3://anvil.gi.ucsc.edu/
 export SRCDIR=public/
-aws s3 sync --acl public-read $SRCDIR $BUCKET --delete --profile platform-anvil-dev
+aws s3 sync $SRCDIR $BUCKET --delete --profile platform-anvil-dev
 aws cloudfront create-invalidation --distribution-id E2DEJ1QLYBYKJ4 --paths "/*" --profile platform-anvil-dev
