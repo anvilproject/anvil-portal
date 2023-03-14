7c6e933b237558d52d05f0b7ccfe0364ff8f2379
Dave Rogers <dave@clevercanary.com>
2023-03-14 00:35:11 -0700

    update anvil cgl deploy script

--- cgl-anvil-dev-deploy.sh
+++ cgl-anvil-dev-deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-#gatsby clean
-#npm run build
-
+n 16.15.1
+npm ci
+gatsby clean
+npm run build
 
 export BUCKET=s3://anvil.gi.ucsc.edu/
 export SRCDIR=public/
 aws s3 sync $SRCDIR $BUCKET --delete --profile platform-anvil-dev
-aws cloudfront create-invalidation --distribution-id E2DEJ1QLYBYKJ4 --paths "/*" --profile platform-anvil-dev
+aws cloudfront create-invalidation --distribution-id E3JAUIVOC72EMP --paths "/*" --profile platform-anvil-dev
