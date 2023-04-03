90a4791709af4d486525adf84c24aaabd50cd190
hunterckx <118154470+hunterckx@users.noreply.github.com>
2023-04-02 19:30:52 -0700

    chore: update ingestion chart (#2629)

--- scripts/update-ingestion-chart.js
+++ scripts/update-ingestion-chart.js
@@ -2,12 +2,13 @@ const path = require("path");
 const fsPromises = require("fs/promises");
 const { promisify } = require("util");
 const parseCsv = promisify(require("csv-parse").parse);
-const { startYear, monthDataByConsortium: oldMonthData} = require("./chart-data-2022-05.js");
+
+const chartDataPath = "../src/components/data-ingestion-chart/chart-data.js";
+const { startYear, endDateNum: oldEndDateNum, monthDataByConsortium: oldMonthData } = require(chartDataPath);
 
 const workspaceFilesPath = "./workspace-files";
 const workspacesInfoPath = "../plugins/utils/dashboard-source-anvil.tsv";
-const outPath = "../src/components/data-ingestion-chart/chart-data.ts";
-const newDataMinTime = new Date(2022, 7, 26).getTime();
+const newDataMinTime = oldEndDateNum + 1;
 
 generateChartData().then(success => {
 	if (success) console.log("Done");
@@ -36,25 +37,29 @@ async function generateChartData() {
 			entries.forEach(e => {
 				let dateNum = new Date(e.date).getTime();
 				if (dateNum < newDataMinTime) {
-					dateNum = newDataMinTime;
+					dateNum = newDataMinTime; // shift new data to after the old data, so it can cancel out without changing the chart history
 					e = Object.assign({}, e, {date: new Date(dateNum).toISOString()});
 				}
 				arr.push(e);
 				if (dateNum > maxDateNum) maxDateNum = dateNum;
 			});
 		} else {
-			console.log("Missing: " + ws.name);
+			console.log("No file for " + ws.name);
 		}
 	}
-	
+
 	const numMonths = dateToIndex(maxDateNum) + 1;
+  const oldDataEndIndex = oldMonthData[0][1].length - 1;
 	const newDataStartIndex = dateToIndex(newDataMinTime);
+  const minNewStartVals = {};
+
 	const monthDataObj = Object.fromEntries(oldMonthData.map(([consortium, addedSizes]) => {
 		const newAddedSizes = addedSizes.concat(Array(numMonths - addedSizes.length).fill(0));
-		newAddedSizes[newDataStartIndex] = -addedSizes.reduce((a, b) => a + b); // cancel out duplicate info from the new data
+    minNewStartVals[consortium] = newAddedSizes[newDataStartIndex];
+		newAddedSizes[newDataStartIndex] -= addedSizes.reduce((a, b) => a + b); // cancel out duplicate info from the new data
 		return [consortium, newAddedSizes];
 	}));
-	
+  
 	Object.entries(entriesByConsortium).forEach(([consortium, entries]) => {
 		const addedSizes = monthDataObj[consortium] || (monthDataObj[consortium] = Array(numMonths).fill(0));
 		
@@ -62,15 +67,34 @@ async function generateChartData() {
 			addedSizes[dateToIndex(e.date)] += parseFloat(e.size);
 		}
 	});
+  
+  for (const [consortium, addedSizes] of Object.entries(monthDataObj)) {
+    const diff = minNewStartVals[consortium] - addedSizes[newDataStartIndex];
+    if (diff > 0) {
+      // value from new data is too small; try to avoid having the graph go downward
+      const sumAfter = addedSizes.slice(newDataStartIndex + 1).reduce((a, b) => a + b);
+      if (sumAfter >= diff) {
+        // if the subsequent data eventually makes up the difference, distribute the negative change across the data
+        addedSizes[newDataStartIndex] += diff;
+        const scale = (sumAfter - diff) / sumAfter;
+        for (let i = newDataStartIndex + 1; i < addedSizes.length; i++) {
+          addedSizes[i] *= scale;
+        }
+      } else if (newDataStartIndex === oldDataEndIndex && newDataStartIndex + 1 < addedSizes.length) {
+        // if a negative can't be avoided, try to avoid having the final month of the previous version of the graph be lower than it was
+        addedSizes[newDataStartIndex] += diff;
+        addedSizes[newDataStartIndex + 1] -= diff;
+      }
+    }
+  }
 	
-	const monthDataByConsortium = Object.entries(monthDataObj).map(([consortium, addedSizes]) => {
-		console.log("Change with new data for " + consortium + ": " + addedSizes[newDataStartIndex] + " TB");
-		return [consortium, addedSizes];
-	});
+	const monthDataByConsortium = Object.entries(monthDataObj);
+  
+	const scriptText = `module.exports = {startYear: ${JSON.stringify(startYear)}, endDateNum: ${JSON.stringify(maxDateNum)}, monthDataByConsortium: ${JSON.stringify(monthDataByConsortium)}};`;
 	
-	const scriptText = `export const startYear = ${JSON.stringify(startYear)}; export const newDataStartIndex = ${JSON.stringify(newDataStartIndex)}; export const monthDataByConsortium: Array<[string, Array<number>]> = ${JSON.stringify(monthDataByConsortium)};`;
-	
-	await fsPromises.writeFile(outPath, scriptText);
+	await fsPromises.writeFile(chartDataPath, scriptText);
+
+  console.log("Added new data");
 	
 	return true;
 	
