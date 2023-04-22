1e2f163d2c67bf3d4f22c6112a942ed31e71d40d
Dave Rogers <dave@clevercanary.com>
2023-04-21 23:28:49 -0700

    feat: update new anvil prod deploy script

--- cgl-platform-anvil-portal-deploy.sh
+++ cgl-platform-anvil-portal-deploy.sh
@@ -4,13 +4,13 @@ n 16.15.1
 npm ci
 gatsby clean
 
-export GATSBY_ENV="ANVIL-PORTAL-AWS-PROD-CHECKOUT"
+export GATSBY_ENV="PROD"
 export GATSBY_GCSE_CX="anvil"
 export GATSBY_NCPI_GCSE_CX="ncpi"
 
 npm run build-ci
 
-export BUCKET=s3://dfo-anvil-portal.dev/
+export BUCKET=s3://ptm-anvilproject.org/
 export SRCDIR=public/
 aws s3 sync $SRCDIR $BUCKET --delete --profile platform-anvil-portal
-aws cloudfront create-invalidation --distribution-id E170EDMDQRPMH9 --paths "/*" --profile platform-anvil-portal
+aws cloudfront create-invalidation --distribution-id E3PPAMUU6T4255 --paths "/*" --profile platform-anvil-portal
