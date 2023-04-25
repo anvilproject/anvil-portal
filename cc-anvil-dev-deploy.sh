3f6fd5b255a4c04eea9f4611195dffc110252382
Dave Rogers <dave@clevercanary.com>
2023-04-24 21:10:10 -0700

    fix: add environment name to GTM config for clvercanary/data-browser#836

--- cc-anvil-dev-deploy.sh
+++ cc-anvil-dev-deploy.sh
@@ -7,8 +7,10 @@ gatsby clean
 export GATSBY_ENV="ANVIL-PORTAL-CC-DEV"
 export GATSBY_GCSE_CX="anvil"
 export GATSBY_NCPI_GCSE_CX="ncpi"
+
 export GATSBY_GTM_ID="GTM-KMGCR8F"
 export GATSBY_GTM_AUTH="dn4W-jqWUyNBj6ON0Ic_wA"
+export GATSBY_ENV_NAME="env-65"
 
 npm run build-ci
 
