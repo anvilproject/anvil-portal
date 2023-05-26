39b722a89bd57cbe03cb8a57bee6ebd07c3b0210
Fran McDade <frano-m@users.noreply.github.com>
2023-05-27 01:48:09 +1000

    feat: configure consortia page (#2701) (#2702)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

---
+++ next/config/dev/config.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@clevercanary/data-explorer-ui/lib/config/entities";
+
+export const config: Partial<SiteConfig> = {
+  themeOptions: {
+    palette: {
+      primary: {
+        dark: "#003E76",
+        main: "#035C94",
+      },
+    },
+  },
+};
---
+++ next/next.config.mjs
@@ -0,0 +1,85 @@
+import nextMDX from "@next/mdx";
+import withPlugins from "next-compose-plugins";
+import withOptimizedImages from "next-optimized-images";
+import path from "path";
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+});
+
+export default withPlugins(
+  [
+    [
+      withOptimizedImages,
+      {
+        optimizeImagesInDev: true,
+        handleImages: ["jpeg", "png", "svg"],
+        imagesFolder: "images",
+      },
+    ],
+    [
+      withMDX,
+      {
+        pageExtensions: ["md", "mdx", "ts", "tsx"],
+      },
+    ],
+  ],
+  {
+    reactStrictMode: true,
+    images: {
+      disableStaticImages: true,
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      // Add the alias for the peer dependency
+      config.resolve.alias["@emotion/react"] = path.resolve(
+        process.cwd(),
+        "node_modules/@emotion/react"
+      );
+      config.resolve.alias["@emotion/styled"] = path.resolve(
+        process.cwd(),
+        "node_modules/@emotion/styled"
+      );
+      config.resolve.alias["@mui/icons-material"] = path.resolve(
+        process.cwd(),
+        "node_modules/@mui/icons-material"
+      );
+      config.resolve.alias["@mui/material"] = path.resolve(
+        process.cwd(),
+        "node_modules/@mui/material"
+      );
+      config.resolve.alias["@tanstack/react-table"] = path.resolve(
+        process.cwd(),
+        "node_modules/@tanstack/react-table"
+      );
+      config.resolve.alias["axios"] = path.resolve(
+        process.cwd(),
+        "node_modules/axios"
+      );
+      config.resolve.alias["isomorphic-dompurify"] = path.resolve(
+        process.cwd(),
+        "node_modules/isomorphic-dompurify"
+      );
+      config.resolve.alias["match-sorter"] = path.resolve(
+        process.cwd(),
+        "node_modules/match-sorter"
+      );
+      config.resolve.alias["next"] = path.resolve(
+        process.cwd(),
+        "node_modules/next"
+      );
+      config.resolve.alias["react"] = path.resolve(
+        process.cwd(),
+        "node_modules/react"
+      );
+      config.resolve.alias["react-dom"] = path.resolve(
+        process.cwd(),
+        "node_modules/react-dom"
+      );
+      config.resolve.alias["uuid"] = path.resolve(
+        process.cwd(),
+        "node_modules/uuid"
+      );
+      return config;
+    },
+  }
+);
