15a7c6e68a83b19f890bddbda20f53cc3740037e
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-05-27 22:40:13 -0300

    feat: adding header config #5

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -6,16 +6,18 @@ import { CssBaseline } from "@mui/material";
 import { ContentLayout } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/ContentLayout/contentLayout";
 import { Main } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/Main/main.styles";
 import { AppLayout } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/AppLayout/appLayout.styles";
-import { config } from "../config/dev/config";
+import { Header } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/Header/header";
+import config from "../site-config/anvil-portal/config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { themeOptions } = config;
+  const { themeOptions, layout } = config;
   const theme = createAppTheme(themeOptions);
   return (
     <EmotionThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <AppLayout>
+        <Header {...layout.header} />
           <Main>
             <ContentLayout content={<Component {...pageProps} />} />
           </Main>
---
+++ next/site-config/anvil-portal/header.ts
@@ -0,0 +1,72 @@
+import { HeaderProps } from "@clevercanary/data-explorer-ui/lib/components/Layout/components/Header/header";
+import logoAnvil from "../../images/logoAnvil.png";
+import { SOCIAL } from "@clevercanary/data-explorer-ui/lib/components/common/Socials/socials";
+import { ELEMENT_ALIGNMENT } from "@clevercanary/data-explorer-ui/lib/common/entities";
+
+const BROWSER_URL = "https://anvil-portal.dev.clevercanary.com";
+const header: HeaderProps = {
+  logo: { alt: "", link: "", src: logoAnvil, height: 40 },
+  navAlignment: ELEMENT_ALIGNMENT.CENTER,
+  navLinks: [
+    {
+      label: "Overview",
+      url: `${BROWSER_URL}/overview`,
+    },
+    {
+      label: "Learn",
+      url: `${BROWSER_URL}/learn`,
+    },
+    {
+      label: "Datasets",
+      url: "/datasets",
+    },
+    {
+      label: "Consortia",
+      url: "",
+    },
+    {
+      label: "News",
+      url: `${BROWSER_URL}/news`,
+    },
+    {
+      label: "Events",
+      url: `${BROWSER_URL}/events`,
+    },
+    {
+      label: "More",
+      menuItems: [
+        {
+          label: "Team",
+          url: `${BROWSER_URL}/team`,
+        },
+        {
+          label: "FAQ",
+          url: `${BROWSER_URL}/faq`,
+        },
+      ],
+      url: "",
+    },
+  ],
+  socials: [
+    {
+      ...SOCIAL.DISCOURSE,
+      url: "https://help.anvilproject.org/",
+    },
+    {
+      ...SOCIAL.TWITTER,
+      url: "https://twitter.com/useAnVIL",
+    },
+    {
+      ...SOCIAL.YOUTUBE,
+      url: "https://www.youtube.com/channel/UCBbHCj7kUogAMFyBAzzzfUw",
+    },
+    {
+      ...SOCIAL.GITHUB,
+      url: "https://github.com/anvilproject",
+    },
+  ],
+  searchEnabled: true,
+  slogan: "NHGRI Analysis Visualization and Informatics Lab-space",
+};
+
+export default header;
