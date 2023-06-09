de9eb4018fbc3431d56f6432ef5612096566f9ae
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-06-09 11:41:36 +1000

    feat: create publications stub (#2722)

--- next/docs/common/constants.ts
+++ next/docs/common/constants.ts
@@ -2,15 +2,17 @@ import { Breadcrumbs } from "@clevercanary/data-explorer-ui/lib/components/commo
 import { Card } from "@clevercanary/data-explorer-ui/lib/components/common/Card/card";
 import { Grid } from "@clevercanary/data-explorer-ui/lib/components/common/Grid/grid";
 import { ANCHOR_TARGET } from "@clevercanary/data-explorer-ui/lib/components/Links/common/entities";
-import { BreadcrumbsCSER } from "../../components/Breadcrumbs/components/BreadcrumbsCSER/breadcrumbsCSER";
-import { BreadcrumbsNews } from "../../components/Breadcrumbs/components/BreadcrumbsNews/breadcrumbsNews";
-import DataIngestionChart from "../../components/DataIngestionChart/dataIngestionChart";
-import { Image } from "../../components/Image/image";
-import { Subheader } from "../../components/Typography/components/Subheader/subheader";
+import { Image } from "../../components/common/Image/image";
+import { Subheader } from "../../components/common/Typography/components/Subheader/subheader";
+import { BreadcrumbsCSER } from "../../components/Consortia/CSER/components/Breadcrumbs/components/BreadcrumbsCSER/breadcrumbsCSER";
+import { BreadcrumbsNews } from "../../components/Consortia/CSER/components/Breadcrumbs/components/BreadcrumbsNews/breadcrumbsNews";
+import DataIngestionChart from "../../components/Consortia/CSER/components/DataIngestionChart/dataIngestionChart";
+import { Publications } from "../../components/Consortia/CSER/components/Publications/publications";
 
 export const DOC_SITE_FOLDER_NAME = "docs";
 
 export const MDX_COMPONENTS = {
+  Publications,
   Breadcrumbs,
   BreadcrumbsCSER,
   BreadcrumbsNews,
