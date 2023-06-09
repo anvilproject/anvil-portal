48176d22d838d5676157adeec5378bb9a79d49ad
hunterckx <118154470+hunterckx@users.noreply.github.com>
2023-06-09 15:41:40 -0700

    feat: import data onto cser publications page (#2686)

---
+++ next/scripts/convert-cser-publications.js
@@ -0,0 +1,34 @@
+const fsp = require("fs/promises");
+const path = require("path");
+const { promisify } = require("util");
+const parseCsv = promisify(require("csv-parse").parse);
+
+const csvFilePath = "./files/CSER Publications PubMed CSV Save As - 20230523.csv";
+const jsonFilePath = "../components/Consortia/CSER/components/Publications/publications.json";
+
+const columnKeys = {
+  PMID: "pmid",
+  Title: "title",
+  Authors: "authors",
+  Citation: "citation",
+  "First Author": "firstAuthor",
+  "Journal/Book": "journalOrBook",
+  "Publication Year": "publicationYear",
+  "Create Date": "createDate",
+  PMCID: "pmcid",
+  "NIHMS ID": "nihmsId",
+  DOI: "doi",
+};
+
+convert();
+
+async function convert() {
+  const entries = await parseCsv(await fsp.readFile(csvFilePath, "utf8"), {
+    bom: true,
+    columns: names => names.map(name => columnKeys[name]),
+    relax_quotes: true,
+    rtrim: true,
+  });
+  await fsp.writeFile(jsonFilePath, JSON.stringify(entries));
+  console.log(`Exported to ${path.resolve(jsonFilePath)}`);
+}
