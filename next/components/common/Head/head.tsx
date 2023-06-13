4186d69c963bc39ca32496946994cfbb3f347ca9
Fran McDade <frano-m@users.noreply.github.com>
2023-06-13 16:11:35 +1000

    fix: favicons (#2729) (#2752)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- next/components/common/Head/head.tsx
+++ next/components/common/Head/head.tsx
@@ -4,26 +4,26 @@ export const Head = (): JSX.Element => {
   return (
     <NextHead key="page-head">
       <title>The AnVIL</title>
-      <link href="/favicon.ico" rel="icon" sizes="any" />
+      <link href="/consortia/favicons/favicon.ico" rel="icon" sizes="any" />
       <link
-        href="/favicons/favicon-16x16.png"
+        href="/consortia/favicons/favicon-16x16.png"
         rel="icon"
         sizes="16x16"
         type="image/png"
       />
       <link
-        href="/favicons/favicon-32x32.png"
+        href="/favicon-32x32.png"
         rel="icon"
         sizes="32x32"
         type="image/png"
       />
       <link
-        href="/favicons/apple-touch-icon.png"
+        href="/consortia/favicons/apple-touch-icon.png"
         rel="apple-touch-icon"
         sizes="180x180"
         type="image/png"
       />
-      <link href="/favicons/site.webmanifest" rel="manifest" />
+      <link href="/consortia/favicons/site.webmanifest" rel="manifest" />
     </NextHead>
   );
 };
