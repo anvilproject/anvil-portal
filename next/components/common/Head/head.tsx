b3da2231db6775017b7ccd8985f0f1d66a048790
Fran McDade <frano-m@users.noreply.github.com>
2023-06-13 15:28:01 +1000

    fix: favicon (#2729) (#2751)

--- next/components/common/Head/head.tsx
+++ next/components/common/Head/head.tsx
@@ -4,25 +4,26 @@ export const Head = (): JSX.Element => {
   return (
     <NextHead key="page-head">
       <title>The AnVIL</title>
+      <link href="/favicon.ico" rel="icon" sizes="any" />
       <link
+        href="/favicons/favicon-16x16.png"
+        rel="icon"
+        sizes="16x16"
+        type="image/png"
+      />
+      <link
+        href="/favicons/favicon-32x32.png"
+        rel="icon"
+        sizes="32x32"
+        type="image/png"
+      />
+      <link
+        href="/favicons/apple-touch-icon.png"
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicons/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
         type="image/png"
-        sizes="32x32"
-        href="/favicons/favicon-32x32.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicons/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicons/site.webmanifest" />
-      <link rel="icon" href="/favicons/favicon.ico" />
+      <link href="/favicons/site.webmanifest" rel="manifest" />
     </NextHead>
   );
 };
