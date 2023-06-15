68d3f460caece0c3f94410d6ec497e45ced3f886
Dave Rogers <dave@clevercanary.com>
2023-06-15 11:50:43 -0700

    feat: prod deploy script for anvil consortia pages (#2704)

---
+++ next/cgl-anvilproject.consortia.prod-deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo \"Deleting ./out/\"
+rm -rf ./out
+
+echo \"Deleting ./build/\"
+rm -rf ./build
+
+n 16.15.1
+npm ci
+
+mkdir -p build
+
+# Build AnVIL
+rm -rf ./out
+npm run build
+mv out/* build
+
+export BUCKET=s3://ptm-anvilproject.org.consortia/
+export SRCDIR=build/
+
+aws s3 sync  $SRCDIR $BUCKET --delete --profile platform-anvil-portal
+aws cloudfront create-invalidation --distribution-id E3PPAMUU6T4255 --paths "/*" --profile platform-anvil-portal
