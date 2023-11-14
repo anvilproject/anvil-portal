0ec05e3eb3d3ff68e3c9523644966748dbe623a9
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-11-14 23:32:39 +1000

    feat: updated header styles (#2796)

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -2,5 +2,11 @@ import styled from "@emotion/styled";
 import { AppBar } from "@mui/material";
 
 export const Header = styled(AppBar)`
-  border-bottom: 1px solid ${({ theme }) => theme.palette.smoke.main};
+  & {
+    border-bottom: 1px solid ${({ theme }) => theme.palette.smoke.main};
+
+    .MuiToolbar-root {
+      flex: 1;
+    }
+  }
 `;
