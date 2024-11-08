ba8ec32b2a77b9a9d788c1384eae385a4916fc17
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-11-08 13:41:36 +1000

    fix: revert datasets link to catalog (#3336)

--- components/Home/components/Section/components/SectionDatasets/components/Datasets/components/Cards/cards.tsx
+++ components/Home/components/Section/components/SectionDatasets/components/Datasets/components/Cards/cards.tsx
@@ -2,10 +2,7 @@ import { CardSecondaryText } from "@databiosphere/findable-ui/lib/components/com
 import { CardTitle } from "@databiosphere/findable-ui/lib/components/common/Card/components/CardTitle/cardTitle";
 import { ForwardArrowIcon } from "@databiosphere/findable-ui/lib/components/common/CustomIcon/components/ForwardArrowIcon/forwardArrowIcon";
 import { RoundedPaper } from "@databiosphere/findable-ui/lib/components/common/Paper/paper.styles";
-import {
-  ANCHOR_TARGET,
-  REL_ATTRIBUTE,
-} from "@databiosphere/findable-ui/lib/components/Links/common/entities";
+import { ANCHOR_TARGET } from "@databiosphere/findable-ui/lib/components/Links/common/entities";
 import { CardActionArea as MCardActionArea } from "@mui/material";
 import { Fragment } from "react";
 import { SectionCardWithLink } from "../../../../../../../../common/entities";
@@ -23,7 +20,6 @@ export const Cards = ({ cards }: CardsProps): JSX.Element => {
         <Card key={i} component={RoundedPaper}>
           <MCardActionArea
             href={link.url}
-            rel={REL_ATTRIBUTE.NO_OPENER_NO_REFERRER}
             target={link.target || ANCHOR_TARGET.SELF}
           >
             <CardSection>
