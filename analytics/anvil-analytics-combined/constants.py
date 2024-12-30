6adaf647bec0ede50ce7fab4c39968ab34f566df
jpaten <jonah@clevercanary.com>
2024-12-29 18:21:03 -0800

    feat: generated outbound links report using version 3.1.0 (#3465)

--- analytics/anvil-analytics-combined/constants.py
+++ analytics/anvil-analytics-combined/constants.py
@@ -1,3 +1,8 @@
+# CHANGE THESE VALUES TO GENERATE NEW REPORTS
+START_DATE = "2024-11-26"
+END_DATE = "2024-12-26"
+PARENT_FOLDER_NAME = "December 2024 (demos)"
+
 ANVIL_PORTAL_NAME = "anvil-portal"
 ANVIL_CATALOG_NAME = "anvil-catalog"
 ANVIL_EXPLORER_NAME = "anvil-explorer"
@@ -8,12 +13,10 @@ PROPERTY_ID_MAP = {
     ANVIL_PORTAL_NAME: "368678391",
 }
 
-SECRET_NAME = 'GA4_CREDENTIALS'
+SECRET_NAME = 'ANVIL_ANALYTICS_REPORTING_CLIENT_SECRET_PATH'
 GA_PROPERTY_PORTAL = "368678391" # AnVIL Portal - GA4
 PRE_AUDIENCE_EXCLUDE_PAGES_FILTER = "landingPagePlusQueryString!=/guides/content/creating-links"
 EXCLUDE_PAGES_FILTER = {"filter": {"fieldName": "audienceId", "numericFilter": {"operation": "EQUAL", "value": {"doubleValue": 5559548544}}}}
 ANALYTICS_START = "2021-01-01"
-TODAY = 'today'
-THIRTY_DAYS_AGO = '30daysAgo'
 
 OAUTH_PORT = 8082
