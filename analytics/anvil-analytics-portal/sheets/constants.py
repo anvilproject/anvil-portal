3e8e54191316cce673477653b7ad031cae735425
jpaten <jonah@clevercanary.com>
2025-01-21 23:20:42 -0800

    feat: added percent change to analytics sheets notebook (#3465)

--- analytics/anvil-analytics-portal/sheets/constants.py
+++ analytics/anvil-analytics-portal/sheets/constants.py
@@ -1,21 +1,22 @@
 # CHANGE THESE VALUES TO GENERATE NEW REPORTS
-START_DATE = "2024-11-26"
-END_DATE = "2024-12-26"
+# The start and end dates of the current month (yyyy-mm-dd)
+START_DATE_CURRENT = "2024-12-01"
+END_DATE_CURRENT = "2024-12-30"
+# The start and end dates of the prior months
+START_DATE_PRIOR = "2024-11-01"
+END_DATE_PRIOR = "2024-11-30"
+# The name of the folder in which to save the report
 PARENT_FOLDER_NAME = "December 2024 (demos)"
 
-ANVIL_PORTAL_NAME = "anvil-portal"
-ANVIL_CATALOG_NAME = "anvil-catalog"
-ANVIL_EXPLORER_NAME = "anvil-explorer"
+# The name of the spreadsheet with the report
+SHEET_NAME = "AnVIL Portal"
 
-PROPERTY_ID_MAP = {
-    ANVIL_CATALOG_NAME: "368661710",
-    ANVIL_EXPLORER_NAME: "383267328",
-    ANVIL_PORTAL_NAME: "368678391",
-}
+ANVIL_PORTAL_ID = "368678391"
 
 SECRET_NAME = 'ANVIL_ANALYTICS_REPORTING_CLIENT_SECRET_PATH'
 GA_PROPERTY_PORTAL = "368678391" # AnVIL Portal - GA4
 PRE_AUDIENCE_EXCLUDE_PAGES_FILTER = "landingPagePlusQueryString!=/guides/content/creating-links"
+# Excludes users who start on the markdown tutorial page
 EXCLUDE_PAGES_FILTER = {"filter": {"fieldName": "audienceId", "numericFilter": {"operation": "EQUAL", "value": {"doubleValue": 5559548544}}}}
 ANALYTICS_START = "2021-01-01"
 
