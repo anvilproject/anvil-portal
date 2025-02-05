8bfbeb29d80107fb2f007dae918ddbe6c42f0e58
jpaten <jonah@clevercanary.com>
2025-02-04 17:33:09 -0800

    feat: added monthly pageviews and users to analytics sheets (#3536)0

--- analytics/anvil-analytics-portal/sheets/constants.py
+++ analytics/anvil-analytics-portal/sheets/constants.py
@@ -13,6 +13,7 @@ SHEET_NAME = "AnVIL Portal"
 
 ANVIL_PORTAL_ID = "368678391"
 
+HISTORIC_UA_DATA_PATH = "../users_over_time_history.json"
 SECRET_NAME = 'ANVIL_ANALYTICS_REPORTING_CLIENT_SECRET_PATH'
 GA_PROPERTY_PORTAL = "368678391" # AnVIL Portal - GA4
 PRE_AUDIENCE_EXCLUDE_PAGES_FILTER = "landingPagePlusQueryString!=/guides/content/creating-links"
