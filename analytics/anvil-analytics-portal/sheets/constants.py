56fc8d11fb4cfb224fa85ab91207e6e016d16ca7
Jonah Paten <jcpaten@gmail.com>
2025-02-07 18:56:09 -0800

    feat: added monthly pageviews and users to analytics sheets (#3536) (#3552)
    * feat: added monthly pageviews and users to analytics sheets (#3536)0
    
    * chore: bumped analytics package version (#3536)
    
    * fix: standardized analytics_start_date (#3536)
    
    * chore: added analytics chart title (#3536)

--- analytics/anvil-analytics-portal/sheets/constants.py
+++ analytics/anvil-analytics-portal/sheets/constants.py
@@ -13,11 +13,12 @@ SHEET_NAME = "AnVIL Portal"
 
 ANVIL_PORTAL_ID = "368678391"
 
+HISTORIC_UA_DATA_PATH = "../users_over_time_history.json"
 SECRET_NAME = 'ANVIL_ANALYTICS_REPORTING_CLIENT_SECRET_PATH'
 GA_PROPERTY_PORTAL = "368678391" # AnVIL Portal - GA4
 PRE_AUDIENCE_EXCLUDE_PAGES_FILTER = "landingPagePlusQueryString!=/guides/content/creating-links"
 # Excludes users who start on the markdown tutorial page
 EXCLUDE_PAGES_FILTER = {"filter": {"fieldName": "audienceId", "numericFilter": {"operation": "EQUAL", "value": {"doubleValue": 5559548544}}}}
-ANALYTICS_START = "2021-01-01"
+ANALYTICS_START = "2023-07-01"
 
 OAUTH_PORT = 8082
