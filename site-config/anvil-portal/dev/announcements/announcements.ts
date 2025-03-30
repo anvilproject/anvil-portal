89e45133900d63b830e7c0c2cfa7055d4b36c9d0
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2025-03-31 09:23:55 +1000

    feat: remove dev banner for now (#3582)

--- site-config/anvil-portal/dev/announcements/announcements.ts
+++ site-config/anvil-portal/dev/announcements/announcements.ts
@@ -1,19 +1,19 @@
-import { ALERT_PROPS } from "@databiosphere/findable-ui/lib/components/common/Banner/constants";
+// import { ALERT_PROPS } from "@databiosphere/findable-ui/lib/components/common/Banner/constants";
 import {
   ComponentConfig,
   ComponentsConfig,
 } from "@databiosphere/findable-ui/lib/config/entities";
-import * as MDX from "../../../../components/Layout/components/Header/content";
+// import * as MDX from "../../../../components/Layout/components/Header/content";
 import { Banner } from "../../../../components/Home/components/Banner/banner";
 
 export const announcements: ComponentsConfig = [
   {
     component: Banner,
   } as ComponentConfig<typeof Banner>,
-  {
-    component: MDX.DevelopmentModeBanner,
-    props: {
-      ...ALERT_PROPS,
-    },
-  } as ComponentConfig<typeof MDX.DevelopmentModeBanner>,
+  // {
+  //   component: MDX.DevelopmentModeBanner,
+  //   props: {
+  //     ...ALERT_PROPS,
+  //   },
+  // } as ComponentConfig<typeof MDX.DevelopmentModeBanner>,
 ];
